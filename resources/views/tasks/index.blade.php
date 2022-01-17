a0902b6af25ee256063721dc171d1c62e89f48ed
putra-ar <m.putra166175@gmail.com>
2022-01-18 02:08:25 +0700

    remove unnecessary

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,8 +9,6 @@
     <script>
         window.Laravel = { csrfToken: '{{ csrf_token() }}' }
     </script>
-    {{--
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 
